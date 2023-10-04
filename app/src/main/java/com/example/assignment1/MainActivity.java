053e2d4a27af5c0cd9fd2a91c6e479b661c58f7a
Raphaiel <73044874+r-halim@users.noreply.github.com>
2023-10-04 14:25:46 -0400

    Included input validation

--- app/src/main/java/com/example/assignment1/MainActivity.java
+++ app/src/main/java/com/example/assignment1/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,51 +19,56 @@ public class MainActivity extends AppCompatActivity {
 
         //linking to the TD website, when the text view box is clicked
         TextView link = findViewById(R.id.td_reference);
-        link.setOnClickListener(new View.OnClickListener(){
-            public void onClick (View v){
-                Intent intent = new Intent(Intent.ACTION_VIEW,
-                        Uri.parse("https://tools.td.com/mortgage-payment-calculator/"));
-                startActivity(intent);
-            }
+        link.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://tools.td.com/mortgage-payment-calculator/"));
+            startActivity(intent);
         });
     }
 
     //Function for the EMI calculation
-    public void EMI_Calculation(View view){
+    @SuppressLint({"SetTextI18n", "DefaultLocale"}) // to ignore warnings
+    public void EMI_Calculation(View view) {
         double mortgage_principal_amount, interest_input, years_input;
 
-        //Gets mortgage input by user and converts to double
+        // Linking the result to the output_result. Also used to display input errors to the user
+        TextView output_result = (TextView) findViewById(R.id.output_result);
+
+        // Gets all three inputs from user (mortgage amount, interest amount and number of years)
         String mortgage = ((EditText) findViewById(R.id.mortgage_amount)).getText().toString();
-        mortgage_principal_amount = Double.parseDouble(mortgage);
-
-        //Gets interest input by user and converts to double; converting the value input to a decimal, and splits it over 12 months for the monthly interest
         String interest_rate = ((EditText) findViewById(R.id.interest_amount)).getText().toString();
-        interest_input = Double.parseDouble(interest_rate);
-        double interest_rate_monthly = (interest_input / 100) / 12;
-
-        //Gets years input by user and converts to double; converting the years input to months
         String years = ((EditText) findViewById(R.id.years_amount)).getText().toString();
+
+        // Checks if any of the inputs are empty
+        if (mortgage.isEmpty() || interest_rate.isEmpty() || years.isEmpty()) {
+            output_result.setText("Please enter valid values for all inputs.");
+            return; // Exit the method early, since at least 1 input is not valid
+        }
+
+        // Converts all three inputs to double
+        mortgage_principal_amount = Double.parseDouble(mortgage);
+        interest_input = Double.parseDouble(interest_rate);
         years_input = Double.parseDouble(years);
+
+        // Checks if any of the input values are not positive
+        if (mortgage_principal_amount <= 0 || interest_input <= 0 || years_input <= 0) {
+            output_result.setText("Please ensure all inputs are positive.");
+            return; // Exit the method early, since at least 1 input is not valid
+        }
+
+        // Calculates the number of months and monthly interest rate
+        double interest_rate_monthly = (interest_input / 100) / 12;
         double months = years_input * 12;
 
-        /* EMI formula used:
-        (P × R × (1 + R)^N / ((1 + R)^N - 1)
-        P - Principal Amount
-        R - Rate of interest
-        N - loan tenure */
+        // EMI formula used:
+        // (P × R × (1 + R)^N / ((1 + R)^N - 1)
+        // P - Principal Amount
+        // R - Rate of interest
+        // N - loan tenure
 
-        //Calculating EMI
-        double calc_emi = ((mortgage_principal_amount * interest_rate_monthly) * ((Math.pow(1 + interest_rate_monthly, months)) / (Math.pow(1 + interest_rate_monthly, months)-1)));
-
-        //Printing the result to the output_result
-        TextView output_result = (TextView) findViewById(R.id.output_result);
-        //Error catching in the event were the output value is negative
-        if (calc_emi < 0) {
-            output_result.setText("Invalid Input; Please ensure all inputs are positive.");
-        }else{
-            //outputting calculated EMI, formatted to 2 decimal places
-            output_result.setText("The monthly payment would be: \n"+ (String.format("%.2f", calc_emi)));
-        }
+        // Calculating EMI and outputting the result
+        double calc_emi = ((mortgage_principal_amount * interest_rate_monthly) * ((Math.pow(1 + interest_rate_monthly, months)) / (Math.pow(1 + interest_rate_monthly, months) - 1)));
+        output_result.setText("The monthly payment would be: \n" + (String.format("%.2f", calc_emi)));
 
     }
 
